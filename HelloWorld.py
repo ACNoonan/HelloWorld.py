8b51c52f5da63645c8e3e01968fe9c36da2337ab
ACNoonan <adamcnoonan@gmail.com>
2018-11-25 16:19:25 -0500

    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,18 @@
+_author_='dev'
+print('Hello, World!')
+#First use of operators in Python!
+print(1 + 2)
+print(7 * 6)
+#Messing around with a little math
+print()
+print('The End!')
+print("Python's strings are easy to use")
+print('Here we are using "quotes" in a string')
+#How to use quotes within a string - Alternate
+print("hello" + "world")
+#Combining strings into master strings
+greeting = "Hello"
+name = "Adam"
+print(greeting + ' ' + name)
+#Storing strings in variables 
+
